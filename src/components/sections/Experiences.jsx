af7bf536400225d5d86778e318192f36e5d07ec6
ManCat18 <95433607+ManCat18@users.noreply.github.com>
2026-01-26 20:56:38 +0000

    Ajout de l'aternance et mise en page des sujets d'étude

--- src/components/sections/Experiences.jsx
+++ src/components/sections/Experiences.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 
 const experiences = [
     {
+        period: "SEPTEMBRE 2025 - AUJOURD'HUI",
+        title: "Alternance de 3ème année de BUT",
+        company: "Laboratoire de biologie médicale B2A | PFASTATT",
+        subject : "Comparaison et validation de méthodes par PCR pour un changement de kit HPV",
+        tasks: [
+            "Réception et gestion d'échantillons",
+            "Ensemencement des géloses",
+            "Lancement de séries et maintenance des automates de PCR (Starlet et BD max)",
+            "Gestion des stocks et réception de commandes",
+        ]
+    },
+    {
         period: "AVRIL 2025 - JUIN 2025",
         title: "Stage de 2ème année de BUT",
         company: "Friedrich Miescher Institute for Biomedical Research | BÂLE",
+        subject : "Développement du placenta humain et microenvironnement utérin",
         tasks: [
-            "Sujet : Développement du placenta humain et microenvironnement utérin",
             "Recherches bibliographiques",
             "Gestion des stocks",
             "Techniques d’histologie dont microtomie",
@@ -18,6 +30,7 @@ const experiences = [
         period: "JUILLET 2024",
         title: "Opératrice",
         company: "Entreprise SAF | HESINGUE",
+        subject : null,
         tasks: [
             "Préparation et gestion de commande",
             "Assurance qualité",
@@ -29,6 +42,7 @@ const experiences = [
         period: "AOÛT 2022",
         title: "Serveuse polyvalente",
         company: "L'Alcôve | TAGOLSHEIM",
+        subject : null,
         tasks: [
             "Plonge",
             "Service en salle",
@@ -53,6 +67,13 @@ const Experiences = () => {
                             <p className="text-blue-700">{exp.company}</p>
                         </div>
                     </div>
+                    {exp.sujet && (
+                        <div className="border-l-4 border-teal-500 bg-gray-50 rounded-r-lg p-4 pl-6 mb-4">
+                            <p className="text-gray-800 font-medium">
+                                <span className="text-teal-900 font-semibold">Sujet :</span> {exp.sujet}
+                            </p>
+                        </div>
+                    )}
                     <ul className="grid md:grid-cols-2 gap-2">
                         {exp.tasks.map((task, taskIndex) => (
                             <li key={taskIndex} className="flex items-center gap-3 text-gray-700">
